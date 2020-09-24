f6db5fd62441e93bb92d856034c03b4d29369c49
luisricardo.castillejo <luisricardo.castillejo@bbva.com>
2020-09-24 01:30:35 -0500

    Actualizando la referencia del servidor de despleigue

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = require('./app');
 const config = require('./config');
 
 app.listen(config.port, () => {
-  console.log('NodeJS escuchando en el puerto ' + config.port);
+  console.log('(Modularizado) NodeJS escuchando en el puerto ' + config.port);
 });
