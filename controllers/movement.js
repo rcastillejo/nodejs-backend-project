4b67bbd4468969228c2c88868cdb0b1f8054c96e
luisricardo.castillejo <luisricardo.castillejo@bbva.com>
2020-10-03 00:54:49 -0500

    Agregando loistado de movimientos de todas las cuentas

--- controllers/movement.js
+++ controllers/movement.js
@@ -6,7 +6,7 @@ const url = config.mlab_host + config.mlab_db + 'collections/';
 
 function getMovements(request, response) {
   const client = requestJson.createClient(url);
-  let fieldParam = 'f={"_id":0, "movements":1, "iban": 1}';
+  let fieldParam = 'f={"_id":1, "movements":1}';
   client.get(`${config.mlab_collection_account_movements}?${fieldParam}&${config.mlab_key}`,
     function (err, res, body) {
       var msg = {};
@@ -15,7 +15,16 @@ function getMovements(request, response) {
         response.status(500);
       } else {
         if (body.length > 0) {
-          msg = body;
+          msg = new Array();
+          body.map(function (acc) {
+            acc.movements.map(function (mov) {
+              msg.push({
+                "alias": acc._id.$oid,
+                "amount": mov.amount,
+                "date": mov.date
+              });
+            })
+          });
         } else {
           msg = { "msg": "No tiene cuentas disponibles" };
           response.status(404);
