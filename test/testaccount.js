3a35550f2da8b76496c5857712fbbc4538a9608d
luisricardo.castillejo <luisricardo.castillejo@bbva.com>
2020-10-24 14:22:49 -0500

    ordenando los test de cuentas

--- test/testaccount.js
+++ test/testaccount.js
@@ -12,10 +12,7 @@ let user = mocks.user;
 let userId;
 let token;
 
-describe('Agregando un nueva cuenta y eliminandola', () => {
-
-    let alias = "DEMO";
-    let account;
+describe('Cuentas & movimientos', () => {
 
     it(`Creando usuario`, (done) => {
         chai.request('http://localhost:3000')
@@ -47,79 +44,292 @@ describe('Agregando un nueva cuenta y eliminandola', () => {
             })
     })
 
-    it(`Agregando cuenta ${alias}`, (done) => {
-        chai.request('http://localhost:3000')
-            .post('/apitechu/v0/accounts')
-            .set({ "Authorization": `Bearer ${token}` })
-            .send({ "alias": alias })
-            .end((err, res, body) => {
-                console.log('res', res.text);
-                res.status.should.equal(201);
+    describe('Agregando un nueva cuenta y eliminandola', () => {
 
-                res.body.should.have.property('account');
-                res.body.should.have.property('alias');
+        let alias = "DEMO";
+        let account;
 
-                res.body.alias.should.equal(alias);
+        it(`Agregando cuenta ${alias}`, (done) => {
+            chai.request('http://localhost:3000')
+                .post('/apitechu/v0/accounts')
+                .set({ "Authorization": `Bearer ${token}` })
+                .send({ "alias": alias })
+                .end((err, res, body) => {
+                    console.log('res', res.text);
+                    res.status.should.equal(201);
 
-                account = res.body.account;
+                    res.body.should.have.property('account');
+                    res.body.should.have.property('alias');
 
-                done()
-            })
+                    res.body.alias.should.equal(alias);
+
+                    account = res.body.account;
+
+                    done()
+                })
+        })
+
+        it(`Consultando cuenta ${account}`, (done) => {
+            chai.request('http://localhost:3000')
+                .get(`/apitechu/v0/accounts/${account}`)
+                .set({ "Authorization": `Bearer ${token}` })
+                .end((err, res, body) => {
+                    res.status.should.equal(200);
+
+                    res.body.should.have.property('account');
+                    res.body.should.have.property('alias');
+
+                    res.body.account.should.equal(account);
+                    res.body.alias.should.equal(alias);
+
+                    done()
+                })
+        })
+
+        it(`Consultando cuentas`, (done) => {
+            chai.request('http://localhost:3000')
+                .get(`/apitechu/v0/accounts`)
+                .set({ "Authorization": `Bearer ${token}` })
+                .end((err, res, body) => {
+                    res.status.should.equal(200);
+                    res.body.length.should.equal(1);
+
+                    res.body[0].should.have.property('account');
+                    res.body[0].should.have.property('alias');
+
+                    res.body[0].account.should.equal(account);
+                    res.body[0].alias.should.equal(alias);
+
+                    done()
+                })
+        })
+
+        it(`Eliminando cuenta ${account}`, (done) => {
+            chai.request('http://localhost:3000')
+                .delete(`/apitechu/v0/accounts/${account}`)
+                .set({ "Authorization": `Bearer ${token}` })
+                .end((err, res, body) => {
+
+                    res.status.should.equal(200);
+
+                    res.body.should.have.property('account');
+                    res.body.should.have.property('alias');
+
+                    res.body.account.should.equal(account);
+                    res.body.alias.should.equal(alias);
+
+                    done()
+                })
+        })
+
     })
 
-    it(`Consultando cuenta ${account}`, (done) => {
-        chai.request('http://localhost:3000')
-            .get(`/apitechu/v0/accounts/${account}`)
-            .set({ "Authorization": `Bearer ${token}` })
-            .end((err, res, body) => {
-                res.status.should.equal(200);
+    describe.skip('Transfiero entre cuentas', () => {
+        let alias1 = "DEMO1";
+        let alias2 = "DEMO2";
+        let account1;
+        let account2;
 
-                res.body.should.have.property('account');
-                res.body.should.have.property('alias');
+        it(`Agregando cuenta1`, (done) => {
+            chai.request('http://localhost:3000')
+                .post('/apitechu/v0/accounts')
+                .send({ "alias": alias1, "balance": 1.00 })
+                .end((err, res, body) => {
 
-                res.body.account.should.equal(account);
-                res.body.alias.should.equal(alias);
+                    res.status.should.equal(201);
 
-                done()
-            })
+                    res.body.should.have.property('account');
+                    res.body.should.have.property('alias');
+
+                    res.body.alias.should.equal(alias1);
+
+                    account1 = res.body.account;
+
+                    done()
+                })
+        })
+
+        it(`Agregando cuenta2`, (done) => {
+            chai.request('http://localhost:3000')
+                .post('/apitechu/v0/accounts')
+                .send({ "alias": alias2 })
+                .end((err, res, body) => {
+
+                    res.status.should.equal(201);
+
+                    res.body.should.have.property('account');
+                    res.body.should.have.property('alias');
+
+                    res.body.alias.should.equal(alias2);
+
+                    account2 = res.body.account;
+
+                    done()
+                })
+        })
+
+
+        it(`Transfiriendo de cuenta1 a cuenta2`, (done) => {
+            chai.request('http://localhost:3000')
+                .post(`/apitechu/v0/accounts/${account1}/movements`)
+                .send({ "to": account2, "amount": 1.00 })
+                .end((err, res, body) => {
+                    res.status.should.equal(201);
+                    done()
+                })
+        })
+
+
+        it(`Transfiriendo de cuenta2 a cuenta1`, (done) => {
+            chai.request('http://localhost:3000')
+                .post(`/apitechu/v0/accounts/${account2}/movements`)
+                .send({ "to": account1, "amount": 1.00 })
+                .end((err, res, body) => {
+                    res.status.should.equal(201);
+                    done()
+                })
+        })
+
+        it(`Consultando cuenta1`, (done) => {
+            chai.request('http://localhost:3000')
+                .get(`/apitechu/v0/accounts/${account1}`)
+                .end((err, res, body) => {
+                    res.status.should.equal(200);
+                    res.body.should.have.property('balance');
+                    res.body.balance.should.equal(1.00);
+                    done()
+                })
+        })
+
+        it(`Consultando cuenta2`, (done) => {
+            chai.request('http://localhost:3000')
+                .get(`/apitechu/v0/accounts/${account2}`)
+                .end((err, res, body) => {
+                    res.status.should.equal(200);
+                    res.body.should.have.property('balance');
+                    res.body.balance.should.equal(0);
+                    done()
+                })
+        })
+
+        it(`Consultando movimientos de cuenta1`, (done) => {
+            chai.request('http://localhost:3000')
+                .get(`/apitechu/v0/accounts/${account1}/movements`)
+                .end((err, res, body) => {
+                    res.status.should.equal(200);
+                    res.body.length.should.equal(2);
+
+                    res.body[0].should.have.property('to');
+                    res.body[0].should.have.property('amount');
+                    res.body[0].should.have.property('date');
+
+                    res.body[0].to.should.equal(account2);
+                    res.body[0].amount.should.equal(-1.00);
+
+                    res.body[1].should.have.property('from');
+                    res.body[1].should.have.property('amount');
+                    res.body[1].should.have.property('date');
+
+                    res.body[1].from.should.equal(account2);
+                    res.body[1].amount.should.equal(1.00);
+
+                    done()
+                })
+        })
+
+        it(`Consultando movimientos de cuenta2`, (done) => {
+            chai.request('http://localhost:3000')
+                .get(`/apitechu/v0/accounts/${account2}/movements`)
+                .end((err, res, body) => {
+                    res.status.should.equal(200);
+                    res.body.length.should.equal(2);
+
+                    res.body[0].should.have.property('from');
+                    res.body[0].should.have.property('amount');
+                    res.body[0].should.have.property('date');
+
+                    res.body[0].from.should.equal(account1);
+                    res.body[0].amount.should.equal(1.00);
+
+                    res.body[1].should.have.property('to');
+                    res.body[1].should.have.property('amount');
+                    res.body[1].should.have.property('date');
+
+                    res.body[1].to.should.equal(account1);
+                    res.body[1].amount.should.equal(-1.00);
+                    done()
+                })
+        })
+
+        it(`Eliminando cuenta1`, (done) => {
+            chai.request('http://localhost:3000')
+                .delete(`/apitechu/v0/accounts/${account1}`)
+                .end((err, res, body) => {
+                    res.status.should.equal(200);
+                    done()
+                })
+        })
+
+        it(`Eliminando cuenta2`, (done) => {
+            chai.request('http://localhost:3000')
+                .delete(`/apitechu/v0/accounts/${account2}`)
+                .end((err, res, body) => {
+                    res.status.should.equal(200);
+                    done()
+                })
+        })
+
     })
 
-    it(`Consultando cuentas`, (done) => {
-        chai.request('http://localhost:3000')
-            .get(`/apitechu/v0/accounts`)
-            .set({ "Authorization": `Bearer ${token}` })
-            .end((err, res, body) => {
-                res.status.should.equal(200);
-                res.body.length.should.equal(1);
+    describe.skip('Agregando un nueva cuenta ya existente', () => {
 
-                res.body[0].should.have.property('account');
-                res.body[0].should.have.property('alias');
+        let alias = "DEMO";
+        let account;
+        it(`Agregando cuenta ${alias}`, (done) => {
+            chai.request('http://localhost:3000')
+                .post('/apitechu/v0/accounts')
+                .send({ "alias": alias })
+                .end((err, res, body) => {
 
-                res.body[0].account.should.equal(account);
-                res.body[0].alias.should.equal(alias);
+                    res.status.should.equal(201);
 
-                done()
-            })
+                    res.body.should.have.property('account');
+                    res.body.should.have.property('alias');
+
+                    res.body.alias.should.equal(alias);
+
+                    account = res.body.account;
+
+                    done()
+                })
+        })
+
+        it(`Agregando cuenta ${alias} ya existente`, (done) => {
+            chai.request('http://localhost:3000')
+                .post('/apitechu/v0/accounts')
+                .send({ "alias": alias })
+                .end((err, res, body) => {
+
+                    res.status.should.equal(400);
+
+                    res.body.msg.should.equal("Cuenta ya existe");
+
+                    done()
+                })
+        })
+
+        it(`Eliminando cuenta`, (done) => {
+            chai.request('http://localhost:3000')
+                .delete(`/apitechu/v0/accounts/${account}`)
+                .end((err, res, body) => {
+
+                    res.status.should.equal(200);
+
+                    done()
+                })
+        })
+
     })
-
-    it(`Eliminando cuenta ${account}`, (done) => {
-        chai.request('http://localhost:3000')
-            .delete(`/apitechu/v0/accounts/${account}`)
-            .set({ "Authorization": `Bearer ${token}` })
-            .end((err, res, body) => {
-
-                res.status.should.equal(200);
-
-                res.body.should.have.property('account');
-                res.body.should.have.property('alias');
-
-                res.body.account.should.equal(account);
-                res.body.alias.should.equal(alias);
-
-                done()
-            })
-    })
-
 
     it(`Cerrando sesion`, (done) => {
         chai.request('http://localhost:3000')
@@ -148,211 +358,4 @@ describe('Agregando un nueva cuenta y eliminandola', () => {
             })
     })
 
-})
-
-describe.skip('Transfiero entre cuentas', () => {
-    let alias1 = "DEMO1";
-    let alias2 = "DEMO2";
-    let account1;
-    let account2;
-
-    it(`Agregando cuenta1`, (done) => {
-        chai.request('http://localhost:3000')
-            .post('/apitechu/v0/accounts')
-            .send({ "alias": alias1, "balance": 1.00 })
-            .end((err, res, body) => {
-
-                res.status.should.equal(201);
-
-                res.body.should.have.property('account');
-                res.body.should.have.property('alias');
-
-                res.body.alias.should.equal(alias1);
-
-                account1 = res.body.account;
-
-                done()
-            })
-    })
-
-    it(`Agregando cuenta2`, (done) => {
-        chai.request('http://localhost:3000')
-            .post('/apitechu/v0/accounts')
-            .send({ "alias": alias2 })
-            .end((err, res, body) => {
-
-                res.status.should.equal(201);
-
-                res.body.should.have.property('account');
-                res.body.should.have.property('alias');
-
-                res.body.alias.should.equal(alias2);
-
-                account2 = res.body.account;
-
-                done()
-            })
-    })
-
-
-    it(`Transfiriendo de cuenta1 a cuenta2`, (done) => {
-        chai.request('http://localhost:3000')
-            .post(`/apitechu/v0/accounts/${account1}/movements`)
-            .send({ "to": account2, "amount": 1.00 })
-            .end((err, res, body) => {
-                res.status.should.equal(201);
-                done()
-            })
-    })
-
-
-    it(`Transfiriendo de cuenta2 a cuenta1`, (done) => {
-        chai.request('http://localhost:3000')
-            .post(`/apitechu/v0/accounts/${account2}/movements`)
-            .send({ "to": account1, "amount": 1.00 })
-            .end((err, res, body) => {
-                res.status.should.equal(201);
-                done()
-            })
-    })
-
-    it(`Consultando cuenta1`, (done) => {
-        chai.request('http://localhost:3000')
-            .get(`/apitechu/v0/accounts/${account1}`)
-            .end((err, res, body) => {
-                res.status.should.equal(200);
-                res.body.should.have.property('balance');
-                res.body.balance.should.equal(1.00);
-                done()
-            })
-    })
-
-    it(`Consultando cuenta2`, (done) => {
-        chai.request('http://localhost:3000')
-            .get(`/apitechu/v0/accounts/${account2}`)
-            .end((err, res, body) => {
-                res.status.should.equal(200);
-                res.body.should.have.property('balance');
-                res.body.balance.should.equal(0);
-                done()
-            })
-    })
-
-    it(`Consultando movimientos de cuenta1`, (done) => {
-        chai.request('http://localhost:3000')
-            .get(`/apitechu/v0/accounts/${account1}/movements`)
-            .end((err, res, body) => {
-                res.status.should.equal(200);
-                res.body.length.should.equal(2);
-
-                res.body[0].should.have.property('to');
-                res.body[0].should.have.property('amount');
-                res.body[0].should.have.property('date');
-
-                res.body[0].to.should.equal(account2);
-                res.body[0].amount.should.equal(-1.00);
-
-                res.body[1].should.have.property('from');
-                res.body[1].should.have.property('amount');
-                res.body[1].should.have.property('date');
-
-                res.body[1].from.should.equal(account2);
-                res.body[1].amount.should.equal(1.00);
-
-                done()
-            })
-    })
-
-    it(`Consultando movimientos de cuenta2`, (done) => {
-        chai.request('http://localhost:3000')
-            .get(`/apitechu/v0/accounts/${account2}/movements`)
-            .end((err, res, body) => {
-                res.status.should.equal(200);
-                res.body.length.should.equal(2);
-
-                res.body[0].should.have.property('from');
-                res.body[0].should.have.property('amount');
-                res.body[0].should.have.property('date');
-
-                res.body[0].from.should.equal(account1);
-                res.body[0].amount.should.equal(1.00);
-
-                res.body[1].should.have.property('to');
-                res.body[1].should.have.property('amount');
-                res.body[1].should.have.property('date');
-
-                res.body[1].to.should.equal(account1);
-                res.body[1].amount.should.equal(-1.00);
-                done()
-            })
-    })
-
-    it(`Eliminando cuenta1`, (done) => {
-        chai.request('http://localhost:3000')
-            .delete(`/apitechu/v0/accounts/${account1}`)
-            .end((err, res, body) => {
-                res.status.should.equal(200);
-                done()
-            })
-    })
-
-    it(`Eliminando cuenta2`, (done) => {
-        chai.request('http://localhost:3000')
-            .delete(`/apitechu/v0/accounts/${account2}`)
-            .end((err, res, body) => {
-                res.status.should.equal(200);
-                done()
-            })
-    })
-
-})
-
-describe.skip('Agregando un nueva cuenta ya existente', () => {
-
-    let alias = "DEMO";
-    let account;
-    it(`Agregando cuenta ${alias}`, (done) => {
-        chai.request('http://localhost:3000')
-            .post('/apitechu/v0/accounts')
-            .send({ "alias": alias })
-            .end((err, res, body) => {
-
-                res.status.should.equal(201);
-
-                res.body.should.have.property('account');
-                res.body.should.have.property('alias');
-
-                res.body.alias.should.equal(alias);
-
-                account = res.body.account;
-
-                done()
-            })
-    })
-
-    it(`Agregando cuenta ${alias} ya existente`, (done) => {
-        chai.request('http://localhost:3000')
-            .post('/apitechu/v0/accounts')
-            .send({ "alias": alias })
-            .end((err, res, body) => {
-
-                res.status.should.equal(400);
-
-                res.body.msg.should.equal("Cuenta ya existe");
-
-                done()
-            })
-    })
-
-    it(`Eliminando cuenta`, (done) => {
-        chai.request('http://localhost:3000')
-            .delete(`/apitechu/v0/accounts/${account}`)
-            .end((err, res, body) => {
-
-                res.status.should.equal(200);
-
-                done()
-            })
-    })
-
-})
+});
