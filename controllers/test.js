49527150324135ccee1046215ad538a113208a7d
luisricardo.castillejo <luisricardo.castillejo@bbva.com>
2020-10-26 00:12:26 -0500

    Probando despliegue

--- controllers/test.js
+++ controllers/test.js
@@ -1,7 +1,7 @@
 'use strict'
 
 function test(request, response) {
-  response.send("Hola Mundo TechU! Renzo & Richi 4");
+  response.send("Hola Mundo TechU! Renzo & Richi 5");
 }
 
 module.exports = {
